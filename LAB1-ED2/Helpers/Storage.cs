eacaeac6fecdd776f4bbc8361c9570555dd411d4
jamg1906 <jamg1906@gmail.com>
2021-08-24 22:27:39 -0600

    Singleton added

---
+++ LAB1-ED2/Helpers/Storage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB1_ED2.Helpers
+{
+    public class Storage
+    {
+        private static Storage _instance = null;
+
+        public static Storage Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Storage();
+                return _instance;
+            }
+        }
+        //AQUÍ SE DEBERÍA DECLARAR EL ÁRBOL B
+    }
+}
